83d326dacaab74f3954378b10e5aa953668bc73a
Yumkhaibam Dayasankar <dyumkhaubam@evoketechnologies.com>
2023-09-21 15:23:39 +0530

    Add properties file

--- src/main/java/org/daiayum/ftpdemo/FTPService.java
+++ src/main/java/org/daiayum/ftpdemo/FTPService.java
@@ -6,26 +6,31 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FTPService {
-	Logger logger = LoggerFactory.getLogger(getClass());
 
+	@Autowired
+	private Environment env;
+
+	Logger logger = LoggerFactory.getLogger(getClass());	
+	
+	@PostConstruct
 	public void testConnection() throws IOException{
 		logger.info("Starting connection...123");
 
-		String server = "ftp.support.bonitasoft.com";
-        int port = 21;
-        String user = "migration_csc";
-        String pass = "UW@K>DMDJ$Y#l])%yb32";
-
-      //FTPSClient ftpClient = new FTPSClient("TLS", true);
+		//FTPSClient ftpClient = new FTPSClient("TLS", true);
         FTPSClient ftpClient = new FTPSClient("TLS");
         ftpClient.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
         ftpClient.setDefaultTimeout(5*60*1000); // 4 minutes
@@ -35,10 +40,15 @@ public class FTPService {
       //ftpClient.execPBSZ(0);
       //ftpClient.execPROT("P");
 
+        String host = env.getProperty("ftp.host");
+        String userName = env.getProperty("ftp.username");
+        String password = env.getProperty("ftp.password");
+        
 		ftpClient.enterLocalPassiveMode();
-	    ftpClient.connect(server, port);
+		logger.info("Connecting to hoost {}", host);
+		ftpClient.connect(host, 21);
         ftpClient.enterLocalPassiveMode();
-        ftpClient.login(user, pass);
+        ftpClient.login(userName, password);
         logger.info("Logged in: status {}"+ftpClient.getStatus());
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
         try {
--- src/main/java/org/daiayum/ftpdemo/FtpDemoApplication.java
+++ src/main/java/org/daiayum/ftpdemo/FtpDemoApplication.java
@@ -1,16 +1,14 @@
 package org.daiayum.ftpdemo;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FtpDemoApplication {
 
-	public static void mains(String[] args) throws IOException {
+	public static void main(String[] args){
 		SpringApplication.run(FtpDemoApplication.class, args);
-		new FTPService().testConnection();
+	
 	}
-
+	
 }
