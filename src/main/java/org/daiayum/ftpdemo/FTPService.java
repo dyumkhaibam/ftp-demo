181e2222c8ab3cc700153365d053e160835b8cdc
Yumkhaibam Dayasankar <dyumkhaubam@evoketechnologies.com>
2023-09-21 14:20:19 +0530

    COmmit

--- src/main/java/org/daiayum/ftpdemo/FTPService.java
+++ src/main/java/org/daiayum/ftpdemo/FTPService.java
@@ -12,6 +12,7 @@ import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 
 public class FTPService {
 	Logger logger = LoggerFactory.getLogger(getClass());
@@ -28,7 +29,6 @@ public class FTPService {
         FTPSClient ftpClient = new FTPSClient("TLS");
         ftpClient.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
         ftpClient.setDefaultTimeout(5*60*1000); // 4 minutes
-      //ftpClient.setDataTimeout(240000);
         ftpClient.setDataTimeout(Duration.ofMillis(5*60*1000));
         ftpClient.setBufferSize(1024*1024);
         ftpClient.setConnectTimeout(5*60*1000);
@@ -41,33 +41,30 @@ public class FTPService {
         ftpClient.login(user, pass);
         logger.info("Logged in: status {}"+ftpClient.getStatus());
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-        logger.info("**************..."+ftpClient.list());
         try {
             logger.info("&&&&&&&&&&&&&&&&&&&&Reading files...");
-            FTPFile[] files = ftpClient.listFiles("/27745");
-        	logger.info("*********************[GET] Files found on server: '" + files.length + "'");
+            FTPFile[] files = ftpClient.listFiles();
             logger.info("%%%%%%%%%%%%%%%%%%Read files: {}", files != null ? files.length : 0);
-            DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            for (FTPFile file : files) {
-            	logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@Reading files...");
-                String details = file.getName();
-                if (file.isDirectory()) {
-                    details = "[" + details + "]";
-                }
-                details += "\t\t" + file.getSize();
-                details += "\t\t" + dateFormatter.format(file.getTimestamp().getTime());
-                logger.info(details);
-            }	
+            
+            if (files != null && files.length > 0) {
+            	
+                DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                for (FTPFile file : files) {
+                	String details = file.getName();
+                    if (file.isDirectory()) {
+                        details = "[" + details + "]";
+                    }
+                    details += "\t\t" + file.getSize();
+                    details += "\t\t" + dateFormatter.format(file.getTimestamp().getTime());
+                    logger.info("Read file: {}", details);
+                }	
+            }
+            	
         }catch (Exception e){
         	logger.error("Error reading files. Error: {}", e.getMessage());
         }finally {
             ftpClient.logout();
             ftpClient.disconnect();        	
         }
-
- 
-
- 
-
 	}
 }
